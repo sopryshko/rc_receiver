e4dd32fbc24d622bebb21f5ac3c93f4e8cda0471
Sergey Opryshko <sergeioprishko@gmail.com>
2024-04-04 18:11:01 +1100

    Added landing Gears output, replaced two Roll outputs with one (Y-splitter)

--- nano/src/nano.cpp
+++ nano/src/nano.cpp
@@ -5,32 +5,32 @@
 #include "Servo.h"
 
 #define CHAN_NUM 0x7A // radio channel (transmitter = receiver)
-#define LEFT_OUT 2
-#define RIGHT_OUT 3
-#define PITCH_OUT 4
-#define YAW_OUT 5
+#define ROLL_OUT 2
+#define PITCH_OUT 3
+#define YAW_OUT 4
+#define GEARS_OUT 5
 #define THRUST_OUT 6 // PWM pin
 
 RF24 radio(9, 10); // radio module pins
 byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; // possible pipes numbers
 
 Servo engine;
-Servo left_servo;
-Servo right_servo;
+Servo roll_servos;
 Servo pitch_servo;
 Servo yaw_servo;
+Servo gears_servos;
 
-int min_thrust = 1000; // old: 800, VGOOD: 1000, SunnySky: 1000
-int max_thrust = 2000; // old: 2270, VGOOD: 2500, SunnySky: 2000
+int min_thrust = 1000; // SimonK: 800, VGOOD: 1000, SunnySky: 1000
+int max_thrust = 2500; // SimonK: 2270, VGOOD: 2500, SunnySky: 2000
 byte received_data[5];
 
 void outputs_init()
 {
   engine.attach(THRUST_OUT);
-  left_servo.attach(LEFT_OUT);
-  right_servo.attach(RIGHT_OUT);
+  roll_servos.attach(ROLL_OUT);
   pitch_servo.attach(PITCH_OUT);
   yaw_servo.attach(YAW_OUT);
+  gears_servos.attach(GEARS_OUT);
 
   // engine calibration
   engine.writeMicroseconds(max_thrust);
@@ -69,7 +69,7 @@ void setup()
 void print_data()
 {
   size_t data_size = sizeof(received_data) / sizeof(received_data[0]);
-  // mode thrust roll pitch yaw
+  // thrust roll pitch yaw gears
   for (size_t i = 0; i < data_size; i++)
   {
     Serial.print(received_data[i]);
@@ -81,21 +81,21 @@ void print_data()
 
 void write_data()
 {
-  // int mode = received_data[0];
-
-  int thrust_val = map(received_data[1], 0, 255, min_thrust, max_thrust);
+  int thrust_val = map(received_data[0], 0, 255, min_thrust, max_thrust);
   thrust_val = constrain(thrust_val, min_thrust, max_thrust);
   engine.writeMicroseconds(thrust_val);
 
-  int roll_val = received_data[2];
-  left_servo.write(roll_val);
-  right_servo.write(roll_val);
+  int roll_val = received_data[1];
+  roll_servos.write(roll_val);
 
-  int pitch_val = received_data[3];
+  int pitch_val = received_data[2];
   pitch_servo.write(pitch_val);
 
-  int yaw_val = received_data[4];
+  int yaw_val = received_data[3];
   yaw_servo.write(yaw_val);
+
+  int gears_mode = received_data[4];
+  gears_servos.write(gears_mode);
 }
 
 void loop()
